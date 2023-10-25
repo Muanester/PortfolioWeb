415957b654a4f171cc13d4beded2e87da69b2ef7
Muanester <stevenmutisya999@gmail.com>
2023-10-25 19:26:53 +0300

    First commit, Header

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { FaXTwitter } from "react-icons/fa6";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header_socials">
+      <a href="https://linkedin.com" target="blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="blank">
+        <BsGithub />
+      </a>
+      <a href="https://x.com" target="blank">
+        <FaXTwitter />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
