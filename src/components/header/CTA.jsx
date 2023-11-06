9c3bf225788abbf5b0f22802ba14c990c20b9bd8
Muanester <stevenmutisya999@gmail.com>
2023-11-07 01:38:37 +0300

    Deploy

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CV from "../../assets/Stephen_Mutisya_Resume.pdf";
+import CV from "../../assets/Stephen_Mutisya-Resume.pdf";
 import { motion } from "framer-motion";
 
 const CTA = () => {
